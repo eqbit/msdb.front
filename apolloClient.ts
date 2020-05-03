b4cd8a9d52c660fff0e36dd1ab3afbdf95731813
eqbit <eqbits@gmail.com>
2020-05-03 09:50:48 +0500

    fixed cookie

--- apolloClient.ts
+++ apolloClient.ts
@@ -1,15 +1,15 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
 import fetch from 'isomorphic-unfetch';
 import { NextPageContext } from 'next';
 
 export default function createApolloClient(initialState: any, ctx: NextPageContext) {
   return new ApolloClient({
     ssrMode: Boolean(ctx),
-    link: new HttpLink({
-      uri: 'http://localhost:4000/graphql', // Server URL (must be absolute)
-      credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+    link: createHttpLink({
+      uri: 'http://localhost:4000/graphql',
+      credentials: 'include',
       fetch,
     }),
     cache: new InMemoryCache().restore(initialState),
