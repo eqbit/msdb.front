d82f474cf996916d3fbb8a143788c85274de1bdf
eqbit <eqbits@gmail.com>
2020-05-15 23:08:03 +0500

    detail page styles

--- src/components/movies/movie-detail/detail.tsx
+++ src/components/movies/movie-detail/detail.tsx
@@ -7,44 +7,46 @@ type Props = {
 
 const Detail: React.FC<Props> = ({ id }) => {
   return (
-    <GetMovieComponent variables={{ id }}>
-      {({ data }) => {
-        const movie = data?.getMovie;
-        const genres = movie?.genre.split(',');
-        
-        return (
-          <>
-            {movie ? (
-              <>
-                <div className="movie-detail">
-                  <figure className="movie-detail__poster">
-                    <img src={movie.poster} alt=""/>
-                  </figure>
+    <div className="container">
+      <GetMovieComponent variables={{ id }}>
+        {({ data }) => {
+          const movie = data?.getMovie;
+          const genres = movie?.genre.split(',');
+      
+          return (
+            <>
+              {movie ? (
+                <>
+                  <div className="movie-detail">
+                    <figure className="movie-detail__poster">
+                      <img src={movie.poster} alt=""/>
+                    </figure>
                 
-                  <aside className="movie-detail__data">
-                    <h1 className="movie-detail__title">{movie.name}</h1>
+                    <aside className="movie-detail__data">
+                      <h1 className="movie-detail__title">{movie.name}</h1>
                   
-                    <ul className="movie-detail-genres">
-                      {!!genres && genres.map((genre) => (
-                        <li key={genre} className="movie-detail-genres__item">
-                          {genre}
-                        </li>
-                      ))}
-                    </ul>
+                      <ul className="movie-detail-genres">
+                        {!!genres && genres.map((genre) => (
+                          <li key={genre} className="movie-detail-genres__item">
+                            {genre}
+                          </li>
+                        ))}
+                      </ul>
                   
-                    <div className="movie-detail__description">
-                      <p>{movie.description}</p>
-                    </div>
-                  </aside>
-                </div>
-              </>
-            ) : (
-              <span>Loading...</span>
-            )}
-          </>
-        )
-      }}
-    </GetMovieComponent>
+                      <div className="movie-detail__description">
+                        <p>{movie.description}</p>
+                      </div>
+                    </aside>
+                  </div>
+                </>
+              ) : (
+                <span>Loading...</span>
+              )}
+            </>
+          )
+        }}
+      </GetMovieComponent>
+    </div>
   )
 };
 
