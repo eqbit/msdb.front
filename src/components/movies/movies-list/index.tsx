7ce9c8c40959821439192c2be2f6f190edefc008
eqbit <eqbits@gmail.com>
2020-05-16 21:17:05 +0500

    partially redesigned main page

--- src/components/movies/movies-list/index.tsx
+++ src/components/movies/movies-list/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { GetMoviesComponent } from '../../../generated/types.d';
 import { cutText } from '../../../utils/text-utils';
-import Link from 'next/link';
 
 const MoviesList: React.FC = () => {
   let page = 1;
@@ -10,6 +11,8 @@ const MoviesList: React.FC = () => {
   return (
     <GetMoviesComponent variables={{ itemsPerPage, page }}>
       {({ data, fetchMore }) => {
+        
+        // @ts-ignore
         const loadMore = () => {
           fetchMore({
             variables: { itemsPerPage, page: ++page },
@@ -26,30 +29,36 @@ const MoviesList: React.FC = () => {
         return (
           <>
             <div className="container">
-              <ul className="movies-list">
-                {data?.getMovies && data.getMovies.map((movie) => (
-                  <li key={movie.id} className="movies-list__item">
-                    <Link href={`movie/${movie.id}`}>
-                      <a className="movies-list__link" style={{backgroundImage: `url('${movie.poster}')`}}>
-                        <div className="movies-list__rating">
-                          <span>Рейтинг&nbsp;популярности</span>
-                          <span>{movie.popularity}<small>pts</small></span>
-                        </div>
-                        
-                        <div className="movies-list__meta">
-                          <h2 className="movies-list__title">{movie.name}</h2>
-                          
-                          <p className="movies-list__description">
-                            {cutText(movie.description, 80, true)}
-                          </p>
-                        </div>
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+              <div className="movies-list__wrapper">
+                <Scrollbars
+                  universal={true}
+                  style={{ height: 367 }}
+                  renderThumbHorizontal={props => <div {...props} className="movies-list__thumb-horizontal"/>}
+                >
+                  <ul className="movies-list">
+                    {data?.getMovies && data.getMovies.map((movie) => (
+                      <li key={movie.id} className="movies-list__item">
+                        <Link href={`movie/${movie.id}`}>
+                          <a className="movies-list__link" style={{backgroundImage: `url('${movie.poster}')`}}>
+                            <div className="movies-list__rating">
+                              <span>Рейтинг&nbsp;популярности</span>
+                              <span>{movie.popularity}<small>pts</small></span>
+                            </div>
               
-              <button onClick={loadMore}>Load more</button>
+                            <div className="movies-list__meta">
+                              <h2 className="movies-list__title">{movie.name}</h2>
+                
+                              <p className="movies-list__description">
+                                {cutText(movie.description, 80, true)}
+                              </p>
+                            </div>
+                          </a>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </Scrollbars>
+              </div>
             </div>
             
           </>
